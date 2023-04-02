8a900c0da442798e7787a601dfe28d1a99594b3c
Benny Austin <austin.benny@gmail.com>
2023-04-02 16:43:11 +1000

    housekeep

--- dedicated-sqlpools/dw01/dbo/Tables/DimCategories.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [dbo].[DimCategories] (
-    [MovieCategorySK]          TINYINT      NOT NULL,
-    [MovieCategoryDescription] VARCHAR (50) NOT NULL
-)
-WITH (HEAP, DISTRIBUTION = REPLICATE);
-
-
-GO
-
--- dedicated-sqlpools/dw01/dbo/Tables/DimRatings.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [dbo].[DimRatings] (
-    [MovieRatingSK]          TINYINT     NOT NULL,
-    [MovieRatingDescription] VARCHAR (5) NOT NULL
-)
-WITH (HEAP, DISTRIBUTION = REPLICATE);
-
-
-GO
-
